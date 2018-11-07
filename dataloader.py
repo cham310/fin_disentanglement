f7e16cd7c854052076f66beb63b6dd73aee3e652
cham <44227252+cham310@users.noreply.github.com>
2018-11-07 16:49:42 +0900

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -87,7 +87,8 @@ class DailyStockPrice(Dataset):
 
 	def __len__(self):
 		return len(self.data)
- 	def __getitem__(self, idx):
+	
+	def __getitem__(self, idx):
 		return self.data[idx]
 	
 if __name__ =='__main__':
